84152fcd4d9e1a08dec511ef75781d7a1f8f6ff3
gin0115 <glynn.quelch@gmail.com>
2021-08-28 01:04:41 +0100

    created field

---
+++ tests/Fields/Test_Input_Tel.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Input with type PASSWORD tests.
+ *
+ * @author Glynn Quelch <glynn.quelch@gmail.com>
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @package PinkCrab\Form_Fields
+ */
+
+namespace PinkCrab\Form_Fields\Tests\Fields;
+
+use WP_UnitTestCase;
+use PinkCrab\Form_Fields\Abstract_Field;
+use PinkCrab\Form_Fields\Fields\Input_Tel;
+use PinkCrab\Form_Fields\Tests\Trait_Test_Cases\Trait_Length_Tests;
+use PinkCrab\Form_Fields\Tests\Trait_Test_Cases\Trait_Pattern_Tests;
+use PinkCrab\Form_Fields\Tests\Trait_Test_Cases\Trait_Placeholder_Tests;
+use PinkCrab\Form_Fields\Tests\Trait_Test_Cases\Trait_Autocomplete_Tests;
+use PinkCrab\Form_Fields\Tests\Trait_Test_Cases\Trait_General_Field_Tests;
+
+class Test_Input_Tel extends WP_UnitTestCase {
+
+	/**
+	 * Hols the input type
+	 *
+	 * @var string
+	 */
+	protected $field_type = Input_Tel::class;
+
+	/**
+	 * Rendered instance of the field.
+	 *
+	 * @var Abstract_Field
+	 */
+	protected static $field;
+
+	/**
+	 * Include all shared tests via trait.
+	 */
+	use Trait_General_Field_Tests,
+		Trait_Placeholder_Tests,
+		Trait_Autocomplete_Tests,
+		Trait_Pattern_Tests,
+		Trait_Length_Tests;
+
+	/**
+	 * Create new input.
+	 *
+	 * @return void
+	 */
+	public function setup(): void {
+		parent::setup();
+		self::$field = Input_Tel::create( 'key' );
+	}
+
+	/**
+	 * Test the rendered HTML contains the defined values & Keys
+	 *
+	 * @return void
+	 */
+	public function test_html_has_expected_properties(): void {
+		$html = self::$field->as_string();
+		$this->assertStringContainsString( 'type="tel"', $html );
+	}
+
+}
